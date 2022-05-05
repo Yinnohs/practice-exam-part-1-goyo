62ade94178e1de9d5607827ae48ddac584adcd68
josesoto <josesoto@itop.es>
2022-05-05 09:52:12 +0100

    feat: menu created

--- src/CustomQueue.java
+++ src/CustomQueue.java
@@ -2,12 +2,19 @@ import java.util.ArrayList;
 
 public class CustomQueue{
     private ArrayList<Float> values;
+    private int top;
 
     public CustomQueue(){
-
+        this.values = new ArrayList<>();
+        this.top = 0;
     }
     public void enQueue (Float element){
         values.add(element);
+        top ++;
+    }
+
+    public int getTop() {
+        return top;
     }
 
     public  Float deQueue (){
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,43 @@
 import java.util.Scanner;
 
 public class Main {
-    Scanner scanner = new Scanner(System.in);
-    CustomQueue queue = new CustomQueue();
-    boolean stopLooping =  false;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        CustomQueue queue = new CustomQueue();
+        boolean stopLooping =  false;
+        Float total =0f;
+
+        while(!stopLooping){
+            showMenu();
+            String userInput = scanner.nextLine();
+            userInput.toUpperCase();
+
+            System.out.println(userInput.equals("Q"));
+
+            if (userInput.equals("Q")){
+                stopLooping = true;
+            }else {
+                Float number = Float.parseFloat(userInput);
+                queue.enQueue(number);
+            }
+
+        }
+
+        while (queue.getTop() != 0){
+            total += queue.deQueue();
+        }
+
+        System.out.println("the total amount is: " + total);
+
+
+    }
+    public static void showMenu(){
+        System.out.println("--------------");
+        System.out.println("press Q to go out");
+        System.out.println("Or add a number (float number)");
+        System.out.println("--------------");
+
+    }
 }
+
+
